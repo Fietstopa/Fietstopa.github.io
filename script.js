ae0eb32245b7ab671759d437230cff5e48102087
Fietstopa <bmisko984@gmail.com>
2024-10-26 09:17:32 +0200

    changes

--- script.js
+++ script.js
@@ -70,3 +70,16 @@ fieldHolders.forEach((fieldHolder) => {
     }
   });
 });
+
+let burger = document.getElementById("burger"),
+  nav = document.getElementById("main-nav"),
+  slowmo = document.getElementById("slowmo");
+
+burger.addEventListener("click", function (e) {
+  this.classList.toggle("is-open");
+  nav.classList.toggle("is-open");
+});
+
+slowmo.addEventListener("click", function (e) {
+  this.classList.toggle("is-slowmo");
+});
